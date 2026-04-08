8c41247d047721e5e14fd8324ec173f4e8e25c58
ambokileB <wholemike2703email.com>
2026-04-08 19:40:18 +0300

    feat:changes made with requets

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ const allowedOrigins = [
   'http://localhost:5173',   // local dev
   'http://localhost:5174',
   'http://192.168.213.184:5173/',
+  'https://www.betnover.com'
 ];
 app.use(helmet());
 app.use(cors({
